6cb2eb06e164f54d5c594e3db4da14bc76543434
Filipe Nepomuceno <filipearn@yahoo.com.br>
2021-08-04 10:37:44 -0300

    Initial commit

---
+++ src/main/java/arn/filipe/fooddelivery/FooddeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package arn.filipe.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooddeliveryApiApplication.class, args);
+	}
+
+}
